e8f4037f3b73044cc29eb1c4f5ce0779e83c4550
tianjunz user <tianjunz@submit-3.fair-aws-h100-2.hpcaas>
2025-02-20 19:22:06 +0000

    update

--- verl/utils/reward_score/__init__.py
+++ verl/utils/reward_score/__init__.py
@@ -30,8 +30,7 @@ def _default_compute_score(data_source, solution_str, ground_truth, extra_info=N
         from . import prime_math
         res = prime_math.compute_score(solution_str, ground_truth)
     else:
-        # return rllm_reward_fn(data_source, solution_str, ground_truth)
-        return rllm_reward_fn(solution_str, ground_truth)
+        return rllm_reward_fn(data_source, solution_str, ground_truth)
 
     if isinstance(res, (int, float, bool)):
         return float(res)
