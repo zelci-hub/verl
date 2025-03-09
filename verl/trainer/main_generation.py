a7f0b6247a9bdd6614495961c25cba4fdfb8850d
Michael Luo <michael.luo@berkeley.edu>
2025-03-09 04:28:38 +0000

    update generation

--- verl/trainer/main_generation.py
+++ verl/trainer/main_generation.py
@@ -91,7 +91,15 @@ def run_generation(config):
         assert config.rollout.n_val == 1, 'When temperature=0, n_val must be 1.'
 
     # Load Dataset
-    dataset = pd.read_parquet(config.data.path)
+    try:
+        dataset = pd.read_parquet(config.data.path)
+    except Exception as e:
+        config.data.path = config.data.path.replace('.parquet', '.json')
+        print(f"Error loading dataset: {e}")
+        dataset = pd.read_json(config.data.path)
+        
+        
+
     val_dataset = RLHFDataset(parquet_files=config.data.path,
         tokenizer=tokenizer,
         prompt_key=config.data.prompt_key,
