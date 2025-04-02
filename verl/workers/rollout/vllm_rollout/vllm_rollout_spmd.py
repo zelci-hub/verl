0c60a21e9def835bf3ad6db00c039e676a8ca094
Michael Luo <michael.luo@berkeley.edu>
2025-04-02 10:30:41 +0000

    Disable prefix caching, as vllm is bugged

--- verl/workers/rollout/vllm_rollout/vllm_rollout_spmd.py
+++ verl/workers/rollout/vllm_rollout/vllm_rollout_spmd.py
@@ -181,7 +181,7 @@ class vLLMRollout(BaseRollout):
                 disable_log_stats=config.disable_log_stats,
                 max_num_batched_tokens=max_num_batched_tokens,
                 enable_chunked_prefill=config.enable_chunked_prefill,
-                enable_prefix_caching=True,
+                enable_prefix_caching=False,
             )
         # Offload vllm model to reduce peak memory usage
         self.inference_engine.sleep(level=1)
