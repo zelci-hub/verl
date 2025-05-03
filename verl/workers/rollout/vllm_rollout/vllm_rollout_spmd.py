96f5ec86246a76b94f4e7eb14347fb6a7d33dd4d
Michael Luo <michael.luo@berkeley.edu>
2025-05-03 08:59:19 +0000

    lol

--- verl/workers/rollout/vllm_rollout/vllm_rollout_spmd.py
+++ verl/workers/rollout/vllm_rollout/vllm_rollout_spmd.py
@@ -31,7 +31,7 @@ from typing import List
 from contextlib import contextmanager
 from copy import deepcopy
 from types import SimpleNamespace
-from typing import AsyncGenerator, Generator, List, Tuple, TypeVar, Union, Optional
+from typing import AsyncGenerator, Generator, List, Literal, Tuple, TypeVar, Union, Optional
 import asyncio
 import numpy as np
 import torch
@@ -54,9 +54,12 @@ from verl.workers.rollout.vllm_rollout.utils import run_async_generator
 from verl.third_party.vllm import vllm_version
 
 import vllm
-from vllm import AsyncLLMEngine, LLM, SamplingParams, TokensPrompt
+from vllm import AsyncLLMEngine, LLM, SamplingParams, TokensPrompt, LLMEngine
 from vllm.distributed import parallel_state as vllm_ps
 from vllm.engine.arg_utils import AsyncEngineArgs
+from vllm.inputs import SingletonInputs
+from vllm.lora.request import LoRARequest
+from vllm.multimodal.processing import EncDecMultiModalProcessor
 from vllm.outputs import RequestOutput
 
 # Set very long timeout to effectively disable it
@@ -90,10 +93,62 @@ def wake_up_v1(self):
 
 # vLLM 0.8.X implements sleep and wake_up as a coroutine, so we implement a synchronous version.
 def sleep_v2(self, level: int = 1) -> None:
+    self.engine.reset_prefix_cache()
     self.engine.sleep(level)
 
 def wake_up_v2(self, tags: Optional[list[str]] = None) -> None:
     self.engine.wake_up(tags)
+
+def _validate_model_input(
+        self,
+        prompt_inputs: SingletonInputs,
+        lora_request: Optional[LoRARequest],
+        *,
+        prompt_type: Literal["encoder", "decoder"],
+    ):
+    model_config = self.model_config
+    tokenizer = (None if self.tokenizer is None else
+                    self.tokenizer.get_lora_tokenizer(lora_request))
+
+    prompt_ids = prompt_inputs["prompt_token_ids"]
+    if prompt_ids is None or len(prompt_ids) == 0:
+        if prompt_type == "encoder" and model_config.is_multimodal_model:
+            pass  # Mllama may have empty encoder inputs for text-only data
+        else:
+            raise ValueError(f"The {prompt_type} prompt cannot be empty")
+
+    max_prompt_len = self.model_config.max_model_len
+    if len(prompt_ids) > max_prompt_len:
+        if prompt_type == "encoder" and model_config.is_multimodal_model:
+            mm_registry = self.input_preprocessor.mm_registry
+            mm_processor = mm_registry.create_processor(
+                model_config,
+                tokenizer=tokenizer or object(),  # Dummy if no tokenizer
+            )
+            assert isinstance(mm_processor, EncDecMultiModalProcessor)
+
+            if mm_processor.pad_dummy_encoder_prompt:
+                return  # Skip encoder length check for Whisper
+
+        if model_config.is_multimodal_model:
+            suggestion = (
+                "Make sure that `max_model_len` is no smaller than the "
+                "number of text tokens plus multimodal tokens. For image "
+                "inputs, the number of image tokens depends on the number "
+                "of images, and possibly their aspect ratios as well.")
+        else:
+            suggestion = (
+                "Make sure that `max_model_len` is no smaller than the "
+                "number of text tokens.")
+
+        raise ValueError(
+            f"The {prompt_type} prompt (length {len(prompt_ids)}) is "
+            f"longer than the maximum model length of {max_prompt_len}. "
+            f"{suggestion}")
+
+        # TODO: Find out how many placeholder tokens are there so we can
+        # check that chunked prefill does not truncate them
+        # max_batch_len = self.scheduler_config.max_num_batched_tokens
 
 vllm_version = vllm.__version__
 
@@ -110,6 +165,9 @@ else:
         f"vLLM version {vllm_version} sleep/wake_up patching is not explicitly handled. "
         "Please update the code for this version."
     )
+
+LLMEngine._validate_model_input = _validate_model_input
+
 
 
 class vLLMRollout(BaseRollout):
